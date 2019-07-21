047a9a04d32aec7f942626c3919f526a953f781e
Hanif Adams <omeizahanif@gmail.com>
2019-07-21 23:52:11 +0100

    add food article selectors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 
+let appState = {};
+      /*
+        food
+        - name
+        - price
+        - quantity
+      */
+      
+      let addFoodBtn = document.querySelectorAll('.add-tray');
+      
+      const addToTray = (e) => {
+          let foodArticle = document.querySelectorAll('.food-article');
+          let item = {};
+          /*item = foodArticle.map((article) => {
+              
+          });*/
+          console.log('clicked');
+      };
+      
+      addFoodBtn.forEach(function(button) {
+          button.addEventListener('click', addToTray);
+      });
