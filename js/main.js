f31d9d86647570a1a0215a5698d57f36799abac0
Hanif Adams <omeizahanif@gmail.com>
2019-07-23 22:42:23 +0100

    modify addToTray function

--- js/main.js
+++ js/main.js
@@ -10,12 +10,19 @@ let appState = {};
       let addFoodBtn = document.querySelectorAll('.add-tray');
       
       const addToTray = (e) => {
-          let foodArticle = document.querySelectorAll('.food-article');
-          let item = {};
-          /*item = foodArticle.map((article) => {
+          let a;
+          let item = [];
+          let parent = e.target.parentElement;
+          for (let i = 0; i < parent.length; i++) {
               
-          });*/
-          console.log('clicked');
+          }
+          a += '{
+              'food' : parent.children[1].textContent,
+              'price' : parent.children[2].firstChild.textContent,
+              'quantity' : parent.children[2].lastChild.value
+          }';
+          item.push(a);
+          console.log(item);
       };
       
       addFoodBtn.forEach(function(button) {
