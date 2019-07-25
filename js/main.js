0b48f27aea31d9ae4bb02bc7da97be96c885be39
Hanif Adams <omeizahanif@gmail.com>
2019-07-25 21:08:12 +0100

    add createTrayData function

--- js/main.js
+++ js/main.js
@@ -1,29 +1,59 @@
-
 let appState = {};
-let item = [];
-      /*
-        food
-        - name
-        - price
-        - quantity
-      */
-      
-let addFoodBtn = document.querySelectorAll('.add-tray');
-      
+let item = [{
+    food : 'Jollof',
+    price : '$10',
+    quantity: 3
+},
+           {
+    food : 'Beans',
+    price : '$20',
+    quantity: 2
+},
+           {
+    food : 'Fried Rice',
+    price : '$50',
+    quantity: 1
+}];
+
 const addToTray = (e) => {
-          let parent = e.target.parentElement;
-          item.push(order);
-          return item;
+    let parent = e.target.parentElement;
+    let order = {
+        'food' : parent.children[1].textContent,
+        'price' : parent.children[2].firstChild.textContent,
+        'quantity' : parent.children[2].lastChild.value
+    };
+    item.push(order);
+    return item;
 };
 
-const displayTray = () => {};
-const alertUser = ({object}) => {};
+const createTrayData = () => {  
+    let ul = document.createElement('ul');
+    ul.classList.add('tray-item');
+    let li = document.createElement('li');
+    for (let i = 0; i < 4; i++) {
+        ul.append(li);
+    }
+    return ul;
+}
+
+const displayTray = (array) => {
+    let ulElement;
+    let ulDisplay;
+    let data = [...array];
+    let tray = document.querySelector('#tray');
+    for (let i = 0; i < data.length; i++) {
+        ulElement = createTrayData();
+        tray.append(ulElement);
+    }
+    return true;
+           
+};
+const alertUser = () => {};
 const fetchOrder = () => {};
 
 
-      
-
-      
-addFoodBtn.forEach(function(button) {
+let addFoodBtn = document.querySelectorAll('.add-tray');   addFoodBtn.forEach(function(button) {
     button.addEventListener('click', addToTray);
 });
+
+
